8cedd7976c0948b1e6a39e88d3ae9236f84e378b
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-04-14 11:42:14 +0200

    Implementing err message and responses

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require("express"),
-    morgan = require("morgan");
+    morgan = require("morgan"),
+    fs = require("fs"),
+    path = require('path');
 
 const app = express();
 
-app.use(morgan("common"));
+//create write stream (in append mode)
+//a "log.txt" file is created
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { flags: "a" });
+
+//setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
+
+
+//Server response when requesting
+app.get("/", (req, res) => {
+    res.send("Welcome to my app");
+});
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send("Something does not work");
+});
+
+app.listen(8080, () => {
+    console.log("Your app ist listening on port 8080");
+
+});
