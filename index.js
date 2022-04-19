c279ef59a6cd566fa04f14822b79cae3d31dc4e3
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-04-19 18:59:48 +0200

    Reset code to start movie API

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const express = require("express"),
-    morgan = require("morgan"),
-    fs = require("fs"),
-    path = require('path');
+const express = require('express'),
 
-const app = express();
 
-//create write stream (in append mode)
-//a "log.txt" file is created
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { flags: "a" });
 
-//setup the logger
-app.use(morgan("combined", { stream: accessLogStream }));
 
-app.use(express.static(path.join(__dirname, "./public")));
 
-//Server response when requesting
-app.get("/", (req, res) => {
-    res.send("Welcome to my app");
-});
+    app.listen(8080, () => {
+        console.log('Your app ist listening on port 8080');
 
-app.use((err, req, res, next) => {
-    console.log(err.stack);
-    res.status(500).send("Something does not work");
-});
-
-app.listen(8080, () => {
-    console.log("Your app ist listening on port 8080");
-
-});
+    });
