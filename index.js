cb9598c15842a5bfa609ab7812fbecd5f0daaf88
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-04-19 19:03:22 +0200

    Require dependencies

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
+
+app.use(bodyParser.json());
+
+let users = [
+
+]
+
+let movies = [
+
+]
 
 
 
 
 
-    app.listen(8080, () => {
-        console.log('Your app ist listening on port 8080');
+app.listen(8080, () => {
+    console.log('Your app ist listening on port 8080');
 
-    });
+});
