06e445b7c0a58d0d8eb50dd04582bf13ad607f58
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-04-27 11:46:57 +0200

    Add use of bodyparser

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/movieApp', {
     useUnifiedTopology: true
 });
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, './public')));
 
