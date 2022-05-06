dcd253665b246abd79d587213443f73e39a543a4
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-06 13:37:25 +0200

    Change double quotes to single quotes

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ app.use(
             if (!origin) return callback(null, true);
             if (allowedOrigins.indexOf(origin) === -1) {
                 // if specific origin wasn't found on the list
-                let message =
-                    "The CORS policy for this application doesn't allow access from origin " +
-                    origin;
+                let message = `The CORS policy for this application doesn't allow access from origin ' + ${origin}`;
                 return callback(new Error(message), false);
             }
             return callback(null, true);
