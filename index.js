90ed2a0948aa80c45c2e0772cbe438403a8f4dde
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-05-09 11:45:12 +0200

    Allow localhost 1234

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "./public")));
 
 //Restriction of cross origin access app.use needs to be before middleware routes like auth
 let allowedOrigins = [
-    "http://localhost:8080",
+    "http://localhost:1234",
     "https://web-flix-movies.herokuapp.com/",
 ];
 app.use(
