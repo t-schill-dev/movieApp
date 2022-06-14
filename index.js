f0e4c219757969c6bdcefbf49da6cbd623b0a742
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-14 11:57:59 +0200

    Hash password after update user

--- index.js
+++ index.js
@@ -170,11 +170,11 @@ app.put(
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         }
-
+        let hashedPassword = Users.hashPassword(req.body.password);
         Users.findOneAndUpdate({ username: req.params.Username }, {
                 $set: {
                     username: req.body.username,
-                    password: req.body.password,
+                    password: hashedPassword,
                     email: req.body.email,
                     birthday: req.body.birthday,
                 },
